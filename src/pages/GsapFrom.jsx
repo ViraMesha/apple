df0ec073153e64803b0e21d71eb07d6ba89abb4b
ViraMesha <wira.mescha@gmail.com>
2024-04-07 00:32:21 +0300

    feat: Implement the gsap.from() method

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,19 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      y: 250,
+      duration: 2,
+      ease: "power1.inOut",
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+    });
+  }, []);
 
   return (
     <main>
