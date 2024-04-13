f51e1694af857a529c76e4dfda2fa73c80e0f074
ViraMesha <wira.mescha@gmail.com>
2024-04-13 22:15:36 +0300

    feat: Set up model

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,7 +1,30 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import ModelView from "./ModelView";
+import { useRef, useState } from "react";
+import { yellowImg } from "../utils";
+import * as THREE from "three";
 
 const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
+    img: yellowImg,
+  });
+
+  //camera control for the model view
+  const cameraControlSm = useRef();
+  const cameraControlLg = useRef();
+
+  //model
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+
+  //rotation
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
+
   useGSAP(() => {
     gsap.to("#heading", {
       opacity: 1,
@@ -19,7 +42,9 @@ const Model = () => {
 
         {/* Model container */}
         <div className="flex flex-col items-center mt-5">
-          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative"></div>
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView />
+          </div>
         </div>
       </div>
     </section>
