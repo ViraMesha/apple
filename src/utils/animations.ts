9eff5caebac7c9e1176052fa3d3d88e33b35dc6d
ViraMesha <wira.mescha@gmail.com>
2024-04-22 22:45:12 +0300

    feat: Add Features section

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -1,4 +1,8 @@
+import gsap from "gsap";
 import * as THREE from "three";
+
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
 
 export const animateWithGsapTimeline = (
   timeline: gsap.core.Timeline,
@@ -20,4 +24,20 @@ export const animateWithGsapTimeline = (
   timeline.to(firstTarget, { ...animationProps, ease: "power2.inOut" }, "<");
 
   timeline.to(secondTarget, { ...animationProps, ease: "power2.inOut" }, "<");
+};
+
+export const animateWithGsap = (
+  target: string,
+  animationProps: any,
+  scrollProps?: any
+) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverse",
+      start: "top 85%",
+      ...scrollProps,
+    },
+  });
 };
